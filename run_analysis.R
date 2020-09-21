ae3b2d7d3fe71955a9ce81fb18d947857d2e3954
Nudibranches <miki_agustinbr@hotmail.com>
2020-09-21 01:55:58 -0500

    V4 - Metacharacters are a bit confusing, but is working

--- run_analysis.R
+++ run_analysis.R
@@ -39,9 +39,34 @@ Step2<-Step1 %>% select(Subject, Code, contains("mean"), contains("std"))
 ##Step 3:
 #Uses descriptive activity names to name the activities in the data set
 
+#Replacing numerical code with actual descriptions
+Step3<-Step2   #Not necessary, just for coherence with variable names
+Step3$Code <- activities[Step3$Code, 2]
+names(Step3)[2]<-"Activities"   #As is no longer a mysterious number code
+
 ##Step 4:
 #Appropriately labels the data set with descriptive variable names.
+
+#Getting column names readable
+Step4<-Step3   #Not necessary, just for coherence with variable names
+names(Step4)<-gsub("Acc", "Accelerometer", names(Step4))
+names(Step4)<-gsub("Gyro", "Gyroscope", names(Step4))
+names(Step4)<-gsub("BodyBody", "Body", names(Step4))
+names(Step4)<-gsub("Mag", "Magnitude", names(Step4))
+names(Step4)<-gsub("angle", "Angle", names(Step4),ignore.case = TRUE)
+names(Step4)<-gsub("gravity", "Gravity", names(Step4),ignore.case = TRUE)
+names(Step4)<-gsub("tBody", "TimeBody", names(Step4))
+names(Step4)<-gsub("^t", "Time", names(Step4))
+names(Step4)<-gsub("^f", "Frequency", names(Step4))
+names(Step4)<-gsub("mean", "Mean", names(Step4), ignore.case = TRUE)
+names(Step4)<-gsub("std", "STD", names(Step4), ignore.case = TRUE)
+names(Step4)<-gsub("freq", "Frequency", names(Step4), ignore.case = TRUE)
+NameComparisson<-matrix(nrow = 88,ncol = 2)     #Just for sanity check
+NameComparisson[,1]<-names(Step3)
+NameComparisson[,2]<-names(Step4)
 
 ##Step 5:
 #From the data set in step 4, creates a second, independent tidy data set with 
 #the average of each variable for each activity and each subject.
+Step5 <- Step4 %>% group_by(Subject, Activities) %>% summarise_all(list(mean=mean))
+write.table(Step5, "CleanDataSet.txt", row.name=FALSE)
